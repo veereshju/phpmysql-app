5637450325d39a6cbb45f483942dd6aa6ef7cffa
sreepathysois <54054574+sreepathysois@users.noreply.github.com>
2024-01-24 11:44:28 +0530

    Update db.php

--- php/online-shopping-system/db.php
+++ php/online-shopping-system/db.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "db";
-$username = "edureka";
-$password = "admin";
-$db = "database";
+$servername = "localhost";
+$username = "msis";
+$password = "Msois@123";
+$db = "myecommerce";
 
 // Create connection
 $con = mysqli_connect($servername,$username,$password,$db);
